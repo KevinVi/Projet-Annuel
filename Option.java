8f10a155feb3aeb11f2a4d2e00aef0e4af54658c
Saman Mohammad <moha.saman16@gmail.com>
2015-03-19 17:52:46 +0100

    Correction de la taille des boutons du menu de gauche

--- Option.java
+++ Option.java
@@ -3,69 +3,33 @@ package essai;
 /*
  * Classe des bouttons pour les menus
  */
-
 import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.GridLayout;
-import java.awt.TextArea;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JSeparator;
 
-public class Option extends JPanel implements ActionListener{
+
+public class Option extends JPanel {
 	
 	private static final long serialVersionUID = 1L;
-	Essai p = new Essai();
 	
-	public Option() {
+	public Option() 
+	{
+		JButton bouton1 = new JButton("Rectangle");
+		JButton bouton2 = new JButton("Trait");
 		
-		//ImageIcon img = new ImageIcon(getClass().getResource("bouton.png"));
-		JButton bouton1 = new JButton("Ligne");
-		bouton1.setPreferredSize(new Dimension(180,150));
-		
-		JButton bouton2 = new JButton("Rectangle");
-		bouton2.setPreferredSize(new Dimension(180,150));
+		JPanel pan = new JPanel();
+		pan.setLayout(new GridLayout(2,1));
+		pan.add(bouton1);
+		pan.add(bouton2);
 		
 		Box panneauBouton = Box.createVerticalBox();
-		panneauBouton.add(Box.createVerticalStrut(20));
-		panneauBouton.add(bouton1);
+		panneauBouton.add(pan);
 		panneauBouton.add(Box.createVerticalStrut(20));
 		panneauBouton.add(new JSeparator(JSeparator.HORIZONTAL));
-		panneauBouton.add(Box.createVerticalStrut(20));
-		panneauBouton.add(bouton2);
-		panneauBouton.add(Box.createVerticalStrut(20));
-		
+		//panneauBouton.add(Box.createGlue());
 		add(panneauBouton,BorderLayout.CENTER );
-		
-		panneauBouton.add(Box.createGlue());
-		bouton1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent evt) {
-				ActionPerformedBtn1(evt);
-			}
-		});
-		bouton2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent evt) {
-				ActionPerformedBtn2(evt);
-			}
-		});
 	 } 
-	
-	private void ActionPerformedBtn1(ActionEvent evt){
-		//p.paint(getGraphics());
-	}
-	
-	private void ActionPerformedBtn2(ActionEvent evt){
-		//p.paintComponent(getGraphics());
-	}
-	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		
-	}
 }
-
