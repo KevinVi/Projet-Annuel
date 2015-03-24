d4c344069a7f55020449d516ccb6dba2c8ca43ef
KevinVi <kevin.vivor@gmail.com>
2015-03-24 20:02:14 +0100

    Update Option.java
    Ajout boite de dialogue pour le dessin

--- src/menuDessin/Option.java
+++ src/menuDessin/Option.java
@@ -14,20 +14,23 @@ import javax.swing.JSeparator;
 * @author Mohammad Saman, Vivor Kevin
 */
 
-public class Option extends JPanel {
-	
-	private static final long serialVersionUID = 1L;
-	
-	public Option() 
+public class Option extends JPanel implements ActionListener {
+private static final long serialVersionUID = 1L;
+JButton bouton1 ;
+JButton bouton2;
+String[] point = {"3","4","5","6","7","8","9","10"};
+static int sommet;
+Polygon_app poly;
+public Option()
 	{
-		JButton bouton1 = new JButton("Rectangle");
-		JButton bouton2 = new JButton("Trait");
-		
+		bouton1 = new JButton("polygon");
+ 		bouton2 = new JButton("Effacer");
 		JPanel pan = new JPanel();
 		pan.setLayout(new GridLayout(2,1));
 		pan.add(bouton1);
+		bouton1.addActionListener(this);
 		pan.add(bouton2);
-		
+		bouton2.addActionListener(this);
 		Box panneauBouton = Box.createVerticalBox();
 		panneauBouton.add(pan);
 		panneauBouton.add(Box.createVerticalStrut(20));
@@ -36,5 +39,28 @@ public class Option extends JPanel {
 		this.add(panneauBouton,BorderLayout.CENTER );
 		this.setBackground(Color.decode("#2766A1"));
 		//setPreferredSize(new Dimension(180,0));
-	 } 
+	}
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	if(e.getSource()== bouton1){
+		   
+	}
+	if(e.getSource()==bouton2){
+		
+			    
+			    sommet=Integer.parseInt((String) JOptionPane.showInputDialog(null, "Nombre de sommet", "Polygone", JOptionPane.QUESTION_MESSAGE, null, point,point[2]));
+			   /* poly=new Polygon_app();
+			    Option.setDessin(poly);
+				sommet=5;*/
+			    
+	}
+}
+public static int getSommet() {
+	
+	return sommet;
+}
+public static void setSommet(int s) {
+		sommet = s;
+}
 }
