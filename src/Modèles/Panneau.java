eafd3bf171b3b976fe55cb3b6524bad12d7b1a6a
Saman Mohammad <moha.saman16@gmail.com>
2015-05-02 00:44:20 +0200

    Ajout images pour les materiaux

--- src/Modèles/Panneau.java
+++ src/Modèles/Panneau.java
@@ -1,74 +1,97 @@
 package Modèles;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Image;
+import java.net.URL;
 
 import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JToolTip;
 
 import Contrôleur.ResumePanneau;
 
 /**
-* Estimate maker java application with GUI.
-* This class includes radio button for the materials.
-*
-* @author Mohammad Saman, Vivor Kevin
-*/
+ * Estimate maker java application with GUI. This class includes radio button
+ * for the materials.
+ *
+ * @author Mohammad Saman, Vivor Kevin
+ */
 public class Panneau extends JPanel {
-	
-  private String nom;
-  private ButtonGroup bgSol = new ButtonGroup();
-  private JRadioButton jrSol1 = new JRadioButton("Sol 1");
-  private JRadioButton jrSol2 = new JRadioButton("Sol 2");
-  
-  private ButtonGroup bgMur = new ButtonGroup();
-  private static JRadioButton jrMur1 = new JRadioButton("Mur 1");
-  private JRadioButton jrMur2 = new JRadioButton("Mur 2");
-  
-  public Panneau(String nom){
-	    this.nom = nom;
-	  
-	    if(this.nom == "sol")
-	    {
-	    	//Pré-selection d'un bouton        
-	    	//jrSol1.setSelected(true);
-		    //On ajoute les boutons au groupe
-		    bgSol.add(jrSol1);
-		    bgSol.add(jrSol2);
-		    this.setLayout(new GridLayout(2,1));
-		    this.add(jrSol1);
-		    this.add(jrSol2);
-		    this.setPreferredSize(new Dimension(180,0));
-	    }
-	    else
-	    {
-	    	//Pré-selection d'un bouton       
-	    	//jrMur1.setSelected(true);
-		    //On ajoute les boutons au groupe
-	    	bgMur.add(jrMur1);
-	    	bgMur.add(jrMur2);
-		    this.setLayout(new GridLayout(2,1));
-		    this.add(jrMur1);
-		    jrMur1.addActionListener(new ResumePanneau());
-		    this.add(jrMur2);
-		    jrMur2.addActionListener(new ResumePanneau());
-	    }
-  }
-  	public JRadioButton getJrSol1() {
-  		return jrSol1;
+
+	private static final long serialVersionUID = 6986414199300820696L;
+	private String nom;
+
+	private ButtonGroup bgroup = new ButtonGroup();
+	private JRadioButton jrSol1 = new JRadioButton("Tapis doux");
+	private JRadioButton jrSol2 = new JRadioButton("Parquet");
+
+	private JRadioButton jrMur1 = new JRadioButton("Briques");
+	private JRadioButton jrMur2 = new JRadioButton("Baroque");
+
+	private JLabel imgSol1;
+	private JLabel imgSol2;
+	private JLabel imgMur1;
+	private JLabel imgMur2;
+
+	public URL chemin = getClass().getResource("/img/mat/icon/sol1.jpg");
+
+	public Panneau(String nom) {
+		this.nom = nom;
+		this.setPreferredSize(new Dimension(180, 70));
+
+		if (this.nom == "sol") {
+			// On ajoute les boutons au groupe
+			bgroup.add(jrSol1);
+			bgroup.add(jrSol2);
+
+			imgSol1 = new JLabel(new ImageIcon("img/mat/icon/sol1.jpg"));
+			imgSol1.setToolTipText("<html><body><img src='http://cdn4.micasa.ch/medias/sys_master/zoom/c/5/6/7/id_9526227402782_zoom.jpg' width=500 height=500></body></html>");
+
+			imgSol2 = new JLabel(new ImageIcon("img/mat/icon/sol2.jpg"));
+			imgSol2.setToolTipText("<html><img src='http://www.bauwerk-parkett.com/parquet-images/floor/1200x800/3907/parquet-doussie-megapark-long-plank-1250x100x11mm.jpg' width=500 height=500></html>");
+
+			this.setLayout(new GridLayout(2, 2));
+			this.add(jrSol1);
+			this.add(imgSol1);
+
+			this.add(jrSol2);
+			this.add(imgSol2);
+		} else {
+			// On ajoute les boutons au groupe
+			bgroup.add(jrMur1);
+			bgroup.add(jrMur2);
+			imgMur1 = new JLabel(new ImageIcon("img/mat/icon/mur1.jpg"));
+			imgMur1.setToolTipText("<html><body><img src='http://www.scenolia.com/3780/papier-peint-mur-brique.jpg' width=500 height=500></body></html>");
+
+			imgMur2 = new JLabel(new ImageIcon("img/mat/icon/mur2.jpg"));
+			imgMur2.setToolTipText("<html><body><img src='http://st.gdefon.com/wallpapers_original/368847_oboi_tekstura_fon_rabochij-stol_6500x4582_www.Gde-Fon.com.jpg' width=500 height=500></body></html>");
+
+			this.setLayout(new GridLayout(2, 1));
+			this.add(jrMur1);
+			this.add(imgMur1);
+
+			this.add(jrMur2);
+			this.add(imgMur2);
+		}
 	}
-	
+
+	public JRadioButton getJrSol1() {
+		return jrSol1;
+	}
+
 	public JRadioButton getJrSol2() {
 		return jrSol2;
 	}
-	
-	public static JRadioButton getJrMur1() {
+
+	public JRadioButton getJrMur1() {
 		return jrMur1;
 	}
-	
+
 	public JRadioButton getJrMur2() {
 		return jrMur2;
 	}
--- src/pluging/MainFrame.java
+++ src/pluging/MainFrame.java
@@ -1,17 +1,17 @@
 package pluging;
 
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.net.MalformedURLException;
+import java.io.IOException;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.ArrayList;
-
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
@@ -19,110 +19,117 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JTextArea;
 import javax.swing.border.LineBorder;
-import javax.swing.filechooser.FileNameExtensionFilter;
-
 
 /**
- * Frame principale du programme de test des plugins
- * @author Lainé Vincent (dev01, http://vincentlaine.developpez.com/ )
- *
- */
-public class MainFrame extends JFrame implements ActionListener{
+* Estimate maker java application with GUI.
+*
+* @author Mohammad Saman, Vivor Kevin
+*/
+
+public class MainFrame extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 4932662545205980307L;
-	
+
 	private JMenuBar menuBar;
 	private JMenu fileMenu;
-	
+
 	private JMenuItem exitMenuItem;
 	private JMenuItem loadMenuItem;
-	
+
 	private JTextArea TelechargerTextAreaInfo;
-	private PluginsLoader pluginsLoader;
-	
+
 	private JFileChooser f;
-	//private FileNameExtensionFilter filter;
+	// private FileNameExtensionFilter filter;
 	private File dir;
-	private URL loadPath;
-	private URL[] classUrl;
 	private ClassLoader cl;
-	private Class loadedClass;
-	
-	public MainFrame(){
+	private Enumeration<?> enu;
+	private JarFile jarFile;
+	private JarEntry je;
+	private String className;
+	private Class<?> c;
+
+	public MainFrame() {
 		this.initialize();
 	}
-	
-	private void initialize(){
+
+	private void initialize() {
 		this.menuBar = new JMenuBar();
 		this.TelechargerTextAreaInfo = new JTextArea();
-		
+
 		this.fileMenu = new JMenu();
-			this.exitMenuItem = new JMenuItem();
-			this.loadMenuItem = new JMenuItem();
-		//menuBar
+		this.exitMenuItem = new JMenuItem();
+		this.loadMenuItem = new JMenuItem();
+		// menuBar
 		this.menuBar.add(this.fileMenu);
-		
-		//fileMenu
+
+		// fileMenu
 		this.fileMenu.setText("Menu");
 		this.fileMenu.add(this.loadMenuItem);
 		this.fileMenu.addSeparator();
 		this.fileMenu.add(this.exitMenuItem);
-		
+
 		this.TelechargerTextAreaInfo.setBorder(new LineBorder(Color.black));
 		this.TelechargerTextAreaInfo.setEditable(false);
-		this.TelechargerTextAreaInfo.setBackground(Color.decode("#5E8BB5") );
+		this.TelechargerTextAreaInfo.setBackground(Color.decode("#5E8BB5"));
 		this.TelechargerTextAreaInfo.setFont(new Font("Serif", Font.BOLD, 13));
-		
-		//exitMenuItem
+
+		// exitMenuItem
 		this.exitMenuItem.setText("Fermer");
 		this.exitMenuItem.addActionListener(this);
-		
-		//loadMenuItem
+
+		// loadMenuItem
 		this.loadMenuItem.setText("Charger un plugins");
 		this.loadMenuItem.addActionListener(this);
-		
-		//Configuration du JFrame
-		this.setSize(400,300);
+
+		// Configuration du JFrame
+		this.setSize(400, 300);
 		this.setJMenuBar(this.menuBar);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setLayout(new GridLayout(1,1));
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.setLayout(new GridLayout(1, 1));
 		this.getContentPane().add(this.TelechargerTextAreaInfo);
 	}
-	
+
 	public void actionPerformed(ActionEvent e) {
-		
-		if(e.getSource() == this.exitMenuItem ){
-			this.setVisible(false);
-		}
-		else if( e.getSource() == this.loadMenuItem ) {
-				this.f = new JFileChooser();
-				//FileNameExtensionFilter filter = new FileNameExtensionFilter("Fichiers .java", ".java");
-				//f.setFileFilter(filter);
-				
-				if(this.f.showOpenDialog(this) == JFileChooser.APPROVE_OPTION){
-					//this.files.add(f.getSelectedFile().getAbsolutePath());
-					
-					this.dir = new File(this.f.getSelectedFile().getAbsolutePath());
-				
-					try {
-						this.loadPath = this.dir.toURI().toURL();
-						this.classUrl = new URL[]{this.loadPath};
-						
-						this.cl = new URLClassLoader(this.classUrl);
-						this.TelechargerTextAreaInfo.setText("la class télécharger est "+this.cl+". \n Vous pouvez lancer le chargement.");
-						
-						loadedClass = cl.loadClass("testclock"); // must be in package.class name format
-						this.TelechargerTextAreaInfo.setText("la class télécharger est "+this.loadedClass+". \n Le téléchargement a été lancé.");
-					} 
-					catch (MalformedURLException ex) {
-						ex.printStackTrace();
-					} 
-					catch (ClassNotFoundException e1) {
-						e1.printStackTrace();
-					} 
-				}
+
+		if (e.getSource() == this.exitMenuItem) {
+			this.dispose();
+		} else if (e.getSource() == this.loadMenuItem) {
+			this.f = new JFileChooser();
+			// FileNameExtensionFilter filter = new
+			// FileNameExtensionFilter(".jar", ".jar");
+			// f.setFileFilter(filter);
+
+			if (this.f.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+
+				this.dir = new File(this.f.getSelectedFile().getAbsolutePath());
+
+				try {
+					jarFile = new JarFile(this.dir);
+					System.out.println(jarFile);
+					enu = jarFile.entries();
+					URL[] urls = { new URL("jar:file:" + this.dir + "!/") };
+					cl = URLClassLoader.newInstance(urls);
+					System.out.println(cl);
+					while (enu.hasMoreElements()) {
+						je = (JarEntry) enu.nextElement();
+						if (je.isDirectory()|| !je.getName().endsWith(".class")) 
+						{
+							continue;
+						}
+						// -6 because of .class
+						className = je.getName().substring(0,je.getName().length() - 6);
+						className = className.replace('/', '.');
+						System.out.println(className);
+						c = cl.loadClass(className);
+						this.TelechargerTextAreaInfo.setText("la class télécharger est " + this.c+ ". \n");
+					}
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				} catch (ClassNotFoundException e1) {
+					e1.printStackTrace();
+				} 
+			}
 		}
 	}
-	
-}
 
+}
