1d57856469698d340ae45c9e9b78977a947b5fc6
KevinVi <kevin.vivor@gmail.com>
2015-05-03 22:19:30 +0200

    Methode de crétion dynamique de radio , maj polygon

--- src/Modèles/Panneau.java
+++ src/Modèles/Panneau.java
@@ -5,6 +5,7 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.AbstractButton;
 import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
@@ -25,9 +26,9 @@ public class Panneau extends JPanel implements ActionListener {
 	private String nom;
 	private static double prix;
 	private String img;
-	private JRadioButton t;
+	private int j=0;
+	private JRadioButton t,t2,t3,t4,t5,t6,t7,t8,t9,t10,f;
 	private ButtonGroup bgroup = new ButtonGroup();
-	private String tabMatiere[][]= new String[10][10];
 
 	/**
 	 * Constructor of the class Panneau
@@ -46,53 +47,101 @@ public class Panneau extends JPanel implements ActionListener {
 	 * @param i:			total number of radio button for this tab
 	 * 
 	 **/
-	public void AddBoutonPanneau(String title, String imgChemin, String img, int i, int prix){
+	public void AddBoutonPanneau(String title, String imgChemin, String img, int i,int prix){
 		this.setLayout(new GridLayout(i, 1));
-		this.t = new JRadioButton(title);
-		this.bgroup.add(t);
-		this.add(t);
-		this.t.addActionListener(this);
+		//this.t = new JRadioButton(title);
+		
+		f=(JRadioButton) createJ(title,j,prix);
+		System.out.println(j);
+		this.bgroup.add(f);
+		this.add(f);
+		
 		JLabel j = new JLabel(new ImageIcon(imgChemin));
 		this.setImg(imgChemin);
-		this.setPrix(prix);
 		j.setToolTipText("<html><body><img src='"+img+"' width=500 height=500></body></html>");
 		this.add(j);
+	}
+	public  AbstractButton createJ(String nom,int i,int prix){
+		switch (i){
+		case 0:
+			this.t = new JRadioButton(nom);
+			t.addActionListener(this);
+			this.setPrix(prix);
+			j++;
+			return t;
+		case 1:
+			this.t2 = new JRadioButton(nom);
+			t2.addActionListener(this);
+			this.setPrix(prix);
+			j++;
+			return t2;
+		case 2:
+			this.t3 = new JRadioButton(nom);
+			t3.addActionListener(this);
+			this.setPrix(prix);
+			j++;
+			return t3;
+		case 3:
+			this.t4 = new JRadioButton(nom);
+			t4.addActionListener(this);
+			this.setPrix(prix);
+			j++;
+			return t4;
+		case 4:
+			this.t5 = new JRadioButton(nom);
+			t5.addActionListener(this);
+			this.setPrix(prix);
+			j++;
+			return t5;
+		case 5:
+			this.t6 = new JRadioButton(nom);
+			t6.addActionListener(this);
+			j++;
+			return t6;
+		case 6:
+			this.t7 = new JRadioButton(nom);
+			t7.addActionListener(this);
+			j++;
+			return t7;
+		case 7:
+			this.t8 = new JRadioButton(nom);
+			t8.addActionListener(this);
+			j++;
+			return t8;
+		case 8:
+			this.t9 = new JRadioButton(nom);
+			t9.addActionListener(this);
+			j++;
+			return t9;
+		
+		default:
+			this.t10 = new JRadioButton(nom);
+			t10.addActionListener(this);
+			j++;
+			return t10;
+		}
+		
 		
 	}
 	
 	
 	/**
-	 * Setter of the name of the tab
+	 * Setter for the name of the tab
 	 * @param nom :		name of the tab
 	 * 
 	 **/
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
-	
-	/**
-	 * Setter of the price
-	 * @param p :	the price 
-	 * 
-	 **/
+
 	public void setPrix(int p) {
 		this.prix = p;
 	}
 
-	/**
-	 * Getter of the price
-	 * @return prix :	the price 
-	 * 
-	 **/
 	public static double getPrix() {
 		return prix;
 	}
 
-	/**
-	 * Getter of the price
-	 * @return img :	the picture path 
-	 * 
-	 **/
 	public String getImg() {
 		return img;
 	}
@@ -103,7 +152,28 @@ public class Panneau extends JPanel implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource()==t){
 			Polygon.setPrice(getPrix());
+			System.out.println(getPrix());
+		}
+		if(e.getSource()==t2){
+			Polygon.setPrice(getPrix());
+			System.out.println(getPrix());
+		}
+		if (e.getSource() == t3) {
+			Polygon.setPrice(getPrix());
+			System.out.println(getPrix());
+		}
+		if (e.getSource() == t4) {
+			Polygon.setPrice(getPrix());
+			System.out.println(getPrix());
+		}
+		if (e.getSource() == t5) {
+			Polygon.setPrice(getPrix());
+			System.out.println(getPrix());
+		}
+	
 	}
 	
 
--- src/Modèles/Polygon.java
+++ src/Modèles/Polygon.java
@@ -1,89 +1,59 @@
 package Modèles;
+
 import java.awt.BasicStroke;
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.Label;
 import java.awt.Point;
-import java.awt.Rectangle;
 import java.awt.RenderingHints;
-import java.awt.Shape;
-import java.awt.TextArea;
 import java.awt.Toolkit;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import Vue.Menu;
-import Vue.Resume;
 
+public class Polygon extends JPanel implements MouseListener,
+		MouseMotionListener {
 
-public class Polygon extends JPanel implements MouseListener, MouseMotionListener  {
-   
-   /**
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	static int[] xcoord;
+	protected static int[] xcoord;
+	protected static int[] ycoord;
+	private String[] color;
+	protected static double price = 0;
+	protected static int echelle = 100;
+	private int draggedPoint = -1;
+	protected static int sommet = 0;
+	private Image img;
+	private Point mouse = new Point();
 
-	static int[] ycoord;
-	String[] color;
-	private Label label;
-	static double price=0;
-	
+	java.text.DecimalFormat df = new java.text.DecimalFormat("0.##");
 
-	static int echelle=100;
-    int draggedPoint = -1;
-      static int sommet =0;
-      Point2D[] point;
-      JPanel coordonnee ;
-      private static TextArea t;
-      Graphics2D g3;
-      Image img;
-      private Point mouse = new Point();
-     
-      java.text.DecimalFormat df = new java.text.DecimalFormat("0.##");
-      //getSelectedIndex() recupération de l'onglet en cour
-      //Constructeur
-      public Polygon() {
-    	  
-         this.setBackground(Color.WHITE);
-         //this.add(new JLabel(new ImageIcon("img/quadri.jpg")));
-         img = Toolkit.getDefaultToolkit().createImage("img/quadri.jpg");
-         this.addMouseListener(this);
-         this.addMouseMotionListener(this);
-         /*int w =this.getWidth();
+	public Polygon() {
 
- 		int h = this.getHeight();
+		img = Toolkit.getDefaultToolkit().createImage("img/quadri.jpg");
+		this.addMouseListener(this);
+		this.addMouseMotionListener(this);
 
- 		//BufferedImage bi = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
-      
- 		paintImage = new BufferedImage(500,500, BufferedImage.TYPE_3BYTE_BGR);
-         */
-         
-      }
-	
+	}
+
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
 		// Option;
-		
+
 		g.drawImage(img, 0, 0, null);
 
 		if (xcoord == null) {
@@ -96,10 +66,9 @@ public class Polygon extends JPanel implements MouseListener, MouseMotionListene
 		}
 		color = new String[] { "Color.BLUE", "Color.RED", "Color.YELLOW",
 				"Color.BLACK" };
-		// g2.setColor(Color.GREEN);
-		
+
 		g2.fillPolygon(xcoord, ycoord, sommet);
-		
+
 		g2.setColor(Color.BLUE);
 		g2.setStroke(new BasicStroke(2));
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
@@ -109,19 +78,16 @@ public class Polygon extends JPanel implements MouseListener, MouseMotionListene
 			g2.fillRect(xcoord[i] - 3, ycoord[i] - 3, 7, 7);
 
 		}
-		g2.drawString((mouse.x) + ", " + (mouse.y), 5, 13);
+
 		g2.setColor(Color.decode("#2766A1"));
 		g2.drawRect(0, 0, 60, 15);
-		repaint();
-
-		// System.out.println(this.area());
-
-		// affichage des longueur segment
+		g2.setColor(Color.WHITE);
+		g2.fillRect(1, 1, 59, 14);
+		g2.setColor(Color.decode("#2766A1"));
+		g2.drawString((mouse.x) + ", " + (mouse.y), 5, 13);
 
 		g2.setColor(Color.decode("#E90101"));
 		int i = 0;
-		// g2.drawString( ""+df.format(longueurlast(2)) ,moitierXlast(2),
-		// moitierYlast(2));
 		while (i < sommet - 1) {
 			g2.drawString("" + df.format(longueur(i)), moitierX(i), moitierY(i));
 			i++;
@@ -136,203 +102,178 @@ public class Polygon extends JPanel implements MouseListener, MouseMotionListene
 		g2.setColor(Color.WHITE);
 		g2.fillRect(740, 635, 87, 15);
 		g2.setColor(Color.decode("#2766A1"));
-		g2.drawString("Valeur : "+df.format(prix())+" €" , 742, 648);
-        //System.out.println(i);
-        //g2.drawString( ""+df.format(longueurlast(i)) ,moitierXlast(i), moitierYlast(i));
-        
-      }
-	public BufferedImage clip(Shape s, BufferedImage image)
-	{
-	Rectangle r = s.getBounds();
-	int w = r.width;
-	int h = r.height;
-	int type = BufferedImage.TYPE_INT_ARGB_PRE;
-	BufferedImage dst = new BufferedImage(w, h, type);
-	Graphics2D g2 = dst.createGraphics();
-	g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
-	g2.translate(-r.x, -r.y);
-	g2.setClip(s);
-	g2.drawImage(image, 0, 0, null);
-	g2.dispose();
-	return dst;
+		g2.drawString("Valeur : " + df.format(prix()) + " €", 742, 648);
+		repaint();
 	}
-      private int scaleX(double x) {
-         return (int)(x * getWidth());
-      }
-      
-      private int scaleY(double y) {
-         return (int)(y * getHeight());
-      }
-      
-      public void mousePressed(MouseEvent e) {
-         draggedPoint = -1;
-         for (int i = 0; i < sommet; i++) {
-            if (Math.abs(xcoord[i] - e.getX()) < 4 && Math.abs(ycoord[i] - e.getY()) < 4) {
-               draggedPoint = i;
-               break;
-            }
-         }
-         Menu.getSave().setEnabled(true);
-      }
-      
-      public void mouseDragged(MouseEvent e) {
-         if (draggedPoint < 0)
-            return;
-         int x = Math.max(0, Math.min(e.getX(),getWidth()));
-         int y = Math.max(0, Math.min(e.getY(),getHeight()));
-         xcoord[draggedPoint] = x;
-         ycoord[draggedPoint] = y;
-         
-         repaint();
-      // System.out.println(xcoord[draggedPoint]+" et "+ycoord[draggedPoint]);
-       System.out.println(draggedPoint);
-       System.out.println(this.area());
-       
-       
-       
-       
-       
-      }
-      
-      public void setSommet(int s){
-    	  this.sommet =s;
-      }
-      
-      public void mouseReleased(MouseEvent e) { }
-      public void mouseMoved(MouseEvent e) {  mouse = e.getPoint();
-      e.getComponent().repaint();}
-      public void mouseClicked(MouseEvent e) { }
-      public void mouseEntered(MouseEvent e) { }
-      public void mouseExited(MouseEvent e) { }
-    
-      public double area() {
-			int i, j, n = sommet;
+
+	private int scaleX(double x) {
+		return (int) (x * getWidth());
+	}
+
+	private int scaleY(double y) {
+		return (int) (y * getHeight());
+	}
+
+	public void mousePressed(MouseEvent e) {
+		draggedPoint = -1;
+		for (int i = 0; i < sommet; i++) {
+			if (Math.abs(xcoord[i] - e.getX()) < 4
+					&& Math.abs(ycoord[i] - e.getY()) < 4) {
+				draggedPoint = i;
+				break;
+			}
+		}
+		Menu.getSave().setEnabled(true);
+	}
+
+	public void mouseDragged(MouseEvent e) {
+		if (draggedPoint < 0)
+			return;
+		int x = Math.max(0, Math.min(e.getX(), getWidth()));
+		int y = Math.max(0, Math.min(e.getY(), getHeight()));
+		xcoord[draggedPoint] = x;
+		ycoord[draggedPoint] = y;
+
+		repaint();
+
+	}
+
+	public void setSommet(int s) {
+		Polygon.sommet = s;
+	}
+
+	public void mouseReleased(MouseEvent e) {
+	}
+
+	public void mouseMoved(MouseEvent e) {
+		mouse = e.getPoint();
+		e.getComponent().repaint();
+	}
+
+	public void mouseClicked(MouseEvent e) {
+	}
+
+	public void mouseEntered(MouseEvent e) {
+	}
+
+	public void mouseExited(MouseEvent e) {
+	}
+
+	public double area() {
+		int i, j, n = sommet;
 		double area = 0;
 
 		for (i = 0; i < n; i++) {
 			j = (i + 1) % n;
 			area += xcoord[i] * ycoord[j];
-			area -=xcoord[j] * ycoord[i];
+			area -= xcoord[j] * ycoord[i];
 		}
 		area /= 2.0;
-		return (Math.abs(area)/(echelle*100));
+		return (Math.abs(area) / (echelle * 100));
+	}
+
+	public double prix() {
+		double area = area();
+		double result = 0;
+		double prix = Panneau.getPrix();
+		result = area * price;
+		
+		//System.out.println(price);
+		return result;
+	}
+
+	public double longueur(int sommet) {
+		double res, lx, ly = 0.0;
+		lx = xcoord[sommet] - xcoord[sommet + 1];
+		ly = ycoord[sommet] - ycoord[sommet + 1];
+		res = Math.sqrt((lx * lx) + (ly * ly));
+		return res / echelle;
+
+	}
+
+	public int moitierX(int sommet) {
+		int lx = 0;
+		lx = xcoord[sommet] - xcoord[sommet + 1];
+		lx = Math.abs(lx);
+		lx = lx / 2;
+		if (xcoord[sommet] < xcoord[sommet + 1]) {
+			lx += xcoord[sommet];
+		} else {
+			lx += xcoord[sommet + 1];
 		}
-      public double prix(){
-    	  double area=area();
-    	  double result =0;
-    	  //int prix = Panneau.getPrix();
-    	  result=area*price;
-    	  System.out.println(price);
-    	  return result;
-      }
-	public double longueur(int sommet){
-	    double res,lx,ly = 0.0;
-	    lx = xcoord[sommet] - xcoord[sommet+1];
-	    ly = ycoord[sommet] - ycoord[sommet+1];
-	    res= Math.sqrt((lx*lx)+(ly*ly));
-	    return res/echelle;
-	
+		return lx;
 	}
-	public int moitierX(int sommet){
-		int lx= 0;
-	    lx = xcoord[sommet] - xcoord[sommet+1];
-	    lx = Math.abs(lx);
-	    lx = lx / 2;
-	    if (xcoord[sommet]<xcoord[sommet+1]){
-       	 lx += xcoord[sommet];
-        }else{
-       	 lx += xcoord[sommet+1];
-        }
-	   // System.out.println("voici lx:"+lx);
-	    return lx;
-	}
-	public int moitierY(int sommet){
-		int ly= 0;
-		ly = ycoord[sommet] - ycoord[sommet+1];
+
+	public int moitierY(int sommet) {
+		int ly = 0;
+		ly = ycoord[sommet] - ycoord[sommet + 1];
 		ly = Math.abs(ly);
-	    ly = ly / 2;
-	    if (ycoord[sommet]<ycoord[sommet+1]){
-       	 ly += ycoord[sommet];
-        }else{
-       	 ly += ycoord[sommet+1];
-        }
-	   // System.out.println("voici ly:"+ly);
-	    return ly;
+		ly = ly / 2;
+		if (ycoord[sommet] < ycoord[sommet + 1]) {
+			ly += ycoord[sommet];
+		} else {
+			ly += ycoord[sommet + 1];
+		}
+		return ly;
 	}
-	public double longueurlast(int sommet){
-	    double res,lx,ly = 0.0;
-	    lx = xcoord[sommet] - xcoord[0];
-	    ly = ycoord[sommet] - ycoord[0];
-	    res= Math.sqrt((lx*lx)+(ly*ly));
-	    return res/echelle;
-	
+
+	public double longueurlast(int sommet) {
+		double res, lx, ly = 0.0;
+		lx = xcoord[sommet] - xcoord[0];
+		ly = ycoord[sommet] - ycoord[0];
+		res = Math.sqrt((lx * lx) + (ly * ly));
+		return res / echelle;
+
 	}
-	public int moitierXlast(int sommet){
-		int lx= 0;
-	    lx = xcoord[sommet] - xcoord[0];
-	    lx = Math.abs(lx);
-	    lx = lx / 2;
-	    if (xcoord[sommet]<xcoord[0]){
-       	 lx += xcoord[sommet];
-        }else{
-       	 lx += xcoord[0];
-        }
-	  //  System.out.println("voici lx:"+lx);
-	    return lx;
+
+	public int moitierXlast(int sommet) {
+		int lx = 0;
+		lx = xcoord[sommet] - xcoord[0];
+		lx = Math.abs(lx);
+		lx = lx / 2;
+		if (xcoord[sommet] < xcoord[0]) {
+			lx += xcoord[sommet];
+		} else {
+			lx += xcoord[0];
+		}
+		return lx;
 	}
-	public int moitierYlast(int sommet){
-		int ly= 0;
+
+	public int moitierYlast(int sommet) {
+		int ly = 0;
 		ly = ycoord[sommet] - ycoord[0];
 		ly = Math.abs(ly);
-	    ly = ly / 2;
-	    if (ycoord[sommet]<ycoord[0]){
-       	 ly += ycoord[sommet];
-        }else{
-       	 ly += ycoord[0];
-        }
-	   // System.out.println("voici ly:"+ly);
-	    return ly;
+		ly = ly / 2;
+		if (ycoord[sommet] < ycoord[0]) {
+			ly += ycoord[sommet];
+		} else {
+			ly += ycoord[0];
+		}
+		return ly;
 	}
-	public static JSONObject get_json(){
-		int i=0;
-		JSONObject objx = new JSONObject();
-		JSONObject objy = new JSONObject();
-		JSONObject obj2 = new JSONObject();
-		JSONArray ArrayCoorX = new JSONArray();
-		JSONArray ArrayCoorY = new JSONArray();
-		JSONArray ArrayOnglet = new JSONArray();
-		//for (i=0;i<sommet;i++){ pour chaque onglet
-			
-			for (i=0;i<sommet;i++){
-				ArrayOnglet.add(""+xcoord[i]);
-				
-			}
-			for (i=0;i<sommet;i++){
-				ArrayOnglet.add(""+ycoord[i]);
-				
-			}
-			obj2.put("Nom_onglet",ArrayOnglet);
-			//obj2.put("Nom", this.dess)
-			//obj.put("Sommet "+i,"Coordonné X :"+xcoord[i] +", Coordonnée Y : "+ycoord[i]);
-		//}
-		return obj2;
-	}
-	public  JSONObject create_devis(){
 
-		JSONObject obj = new JSONObject();
-		Dimension size = new Dimension(100,100);
-		BufferedImage myImage = new BufferedImage(size.width, size.height, BufferedImage.TYPE_INT_RGB);
-		JOptionPane.showMessageDialog(null, new JLabel(new ImageIcon(myImage)));
-	    return obj ;
+	public static JSONObject get_json() {
+		int i = 0;
+
+		JSONObject obj2 = new JSONObject();
+		JSONArray ArrayOnglet = new JSONArray();
+
+		for (i = 0; i < sommet; i++) {
+			ArrayOnglet.add("" + xcoord[i]);
+
+		}
+		for (i = 0; i < sommet; i++) {
+			ArrayOnglet.add("" + ycoord[i]);
+
+		}
+		obj2.put("Nom_onglet", ArrayOnglet);
+		return obj2;
 	}
 
 	public BufferedImage createImage(JPanel panel) {
 
-		int w = panel.getWidth();
-
-		int h = panel.getHeight();
-
-		BufferedImage bi = new BufferedImage(830,653, BufferedImage.TYPE_INT_ARGB);
+		BufferedImage bi = new BufferedImage(830, 653,
+				BufferedImage.TYPE_INT_ARGB);
 
 		Graphics g = bi.getGraphics();
 
@@ -341,9 +282,6 @@ public class Polygon extends JPanel implements MouseListener, MouseMotionListene
 		return (bi);
 
 	}
-	
-
-	
 
 	public double getPrice() {
 		return price;
@@ -352,56 +290,5 @@ public class Polygon extends JPanel implements MouseListener, MouseMotionListene
 	public static void setPrice(double prix) {
 		price = prix;
 	}
-      /*
-       * Fonction de calcul d'aire
-      public static double area(Point2D[] polyPoints) {
-  		int i, j, n = polyPoints.length;
-  		double area = 0;
-
-  		for (i = 0; i < n; i++) {
-  			j = (i + 1) % n;
-  			area += polyPoints[i].getX() * polyPoints[j].getY();
-  			area -= polyPoints[j].getX() * polyPoints[i].getY();
-  		}
-  		area /= 2.0;
-  		return (area);
-  		public double area() {
-  			int i, j, n = sommet;
-  		double area = 0;
-
-  		for (i = 0; i < n; i++) {
-  			j = (i + 1) % n;
-  			area += xcoord[i] * ycoord[j];
-  			area -=xcoord[j] * ycoord[i];
-  		}
-  		area /= 2.0;
-  		return (area);
-  		}
-  	}*/
-      /*
-       public double area() {
-        double sum = 0.0;
-        for (int i = 0; i < 5; i++) {
-            sum = sum + (xcoord[i] * ycoord[i+1]) - (ycoord[i] * xcoord[i+1]);
-        }
-        return 0.5 * sum;
-    }
-       */
-	 /* public double area() {
-    double sum = 0.0;
-    for (int i = 0; i < sommet; i++) {
-        sum = sum + (xcoord[i] * ycoord[i+1]) - (ycoord[i] * xcoord[i+1]);
-    }
-    return 0.5 * sum;
-}*/
-
-  
-   
-   
-  
-
- 
-
 
 }
-
